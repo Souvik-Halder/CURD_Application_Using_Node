235f2c0e6b527427539b152965d29f0846a04e77
Souvik Halder <souvik.halder2020@gift.edu.in>
2022-08-10 11:43:15 +0530

    first commit consist of initialization, setup

---
+++ main.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const session=require('express-session');
+
+
+
+const PORT=process.env.PORT;
+
+//database connection
+mongoose.connect(process.env.DB_URI,{useNewUrlParser:true})
+.then(()=>{
+    console.log("Mongodb connected");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+//middlewares
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use(session({
+    secret: 'my secret key',
+    saveUninitialized:true,
+    resave:false
+
+}))
+app.use((req,res,next)=>{
+    res.locals.message=req.session.message,
+    delete req.session.message;
+    next();
+})
+
+
+//set template engine
+app.set('view engine','ejs');
+
+
+//routes
+app.use('/',require('./routes/routes'))
+
+
+
+
+
+
+app.listen(PORT,(err)=>{
+    console.log(`server running at ${PORT}`)
+})
---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/',(req,res)=>{
+    res.send("Home page");
+})
+
+router.get('/users',(req,res)=>{
+    res.send("All users");
+})
+
+module.exports=router;
